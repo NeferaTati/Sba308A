810f6f8d93ad4ad64283955b8a5bb75957a5d9bc
NeferaTati <andersoneferatari@gmail.com>
2024-06-18 10:46:45 -0500

    more stuff

--- app.js
+++ app.js
@@ -54,3 +54,11 @@ function displayRes(results) {
     const gallery = document.getElementById('gallery');
     gallery.innerHTML = results.map(item => `<div class="item">${item.name}</div>`).join('');
 }
+// this will make the pages, "pagination"
+
+function makepages(){
+    const pagination = document.getElementById('pagination');
+    pagination.innerHTML = '';
+    const totalPages = Math.ceil(total / perPag);
+
+}
